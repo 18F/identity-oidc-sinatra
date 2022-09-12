5e90b67b7b1a631a6a2cfbd6fb147207ab4299ba
Jeff Sheffel <jscodefix@sheffel.org>
2022-09-12 11:05:44 -0600

    add back requires of active_support core_ext methods
      - apparently these require statements cherry-pick the methods
      - refer to https://guides.rubyonrails.org/active_support_core_extensions.html

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'dotenv/load'
+require 'active_support/core_ext/hash/indifferent_access'
+require 'active_support/core_ext/object/to_query'
 require 'erubi'
 require 'faraday'
 require 'json'
