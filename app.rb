fc30e0463ff0ddb76032841d2287c6a444778a68
Davida Marion <davida.marion@gsa.gov>
2025-04-04 10:36:09 -0400

    Add error handling

--- app.rb
+++ app.rb
@@ -263,9 +263,18 @@ module LoginGov::OidcSinatra
         url: config.attempts_url,
         headers:{'Authorization' => auth })
 
+      
+      response = connection.post
+      if response.status != 200 && ENV['ENABLE_LOGGING'] == 'true'
+        # rubocop:disable Layout/LineLength
+        settings.logger.info("got !200 trying to query #{config.attempts_url} ")
+        # rubocop:enable Layout/LineLength
+      end
+      raise AppError.new(response.body) if response.status != 200
+
       sets = JSON.parse(connection.post.body)['sets']
 
-      events = sets.values.map do |jwe|
+      events = sets.values&.map do |jwe|
         JSON.parse(JWE.decrypt(jwe, config.sp_private_key))
       end
  
